140aac19502622d4a4649e8bfee3dcbc687a47e8
muhBilal <mb336450@gmail.com>
2022-12-08 09:45:59 +0700

    home

--- home.php
+++ home.php
@@ -15,53 +15,51 @@ $query = mysqli_query($con, "SELECT * FROM posts");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Postify | Home</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+        integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="js/jquery-3.2.1.min.js" type="text/javascript"></script>
-
 </head>
 
 <body>
-    <h1>Homepage</h1>
-    <br>
-    <div id="content">
-        <?php while($post = mysqli_fetch_assoc($query)): ?>
+    <div class="header">
+        <h1>Homepage</h1>
+        <button href="logout.php" class="btn-danger">Logout</button>
+    </div>
+
+    <div class="container">
+        <?php while ($post = mysqli_fetch_assoc($query)) : ?>
             <div class="card">
-                <div class="card-content">
-                    <div class="image">
-                        <img src="<?= $post['image']; ?>" alt="post-image">
-                    </div>
-                    <div class="text-content">
-                        <div class="heading">
-                            <h2><?= $post['title']; ?></h2>
-                        </div>
-                        <div class="description">
-                            <p><?= $post['description']; ?></p>
-                        </div>
-                    </div>
-                    <div class="like-btn">
+                <img src="<?= $post['image']; ?>" alt="post-image">
+                <div class="action">
+                    <div class="like">
+                        <i onclick="likePost(<?= $post['id']; ?>)" class="fa-solid fa-heart heart"></i>
                         <p>Total like: <?= $post['likes']; ?></p>
-                        <!-- <a href="like.php?like=">Like</a> -->
-                        <button onclick="likePost(<?= $post['id']; ?>)">Like</button>
                     </div>
+                    <i class="fa-solid fa-comment comment"></i>
                 </div>
             </div>
         <?php endwhile; ?>
     </div>
-    <a href="logout.php">Logout</a>
+
+
 
     <script>
-        function likePost(id){
+        function likePost(id) {
             $.ajax({
-                url:'like.php',
-                type:'post',
-                data:{id: id},
-                success:function(response){
+                url: 'like.php',
+                type: 'post',
+                data: {
+                    id: id
+                },
+                success: function(response) {
                     var msg = "";
-                    alert('Success like')
                     location.reload()
                 }
             });
-        }        
+        }
     </script>
 </body>
 
--- index.php
+++ index.php
@@ -2,11 +2,39 @@
 <html>
 
 <head>
-    <title>Login page with jQuery and AJAX</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Postify</title>
     <link href="css/style.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+        integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+
+    <div class="login">
+        <div>
+            <h1>Login</h1>
+            <div id="message"></div>
+            <form>
+                <div class="row">
+                    <label for="email">Email</label>
+                    <input type="text" class="textbox" id="txt_uname" name="txt_uname" placeholder="Username" />
+
+                </div>
+                <div class="row">
+                    <label for="password">Password</label>
+                    <input type="password" class="textbox" id="txt_pwd" name="txt_pwd" placeholder="Password" />
+                </div>
+                <input type="button" value="Submit" name="but_submit" id="but_submit" class="button" />
+            </form>
+        </div>
+    </div>
+
 
     <script src="js/jquery-3.2.1.min.js" type="text/javascript"></script>
-
     <script type="text/javascript">
         $(document).ready(function() {
 
@@ -37,22 +65,6 @@
 
         });
     </script>
-</head>
-
-<body>
-    <h1>Login</h1>
-    <form>
-        <div class="row">
-            <label for="email">Email</label>
-            <input type="text" class="textbox" id="txt_uname" name="txt_uname" placeholder="Username" />
-
-        </div>
-        <div class="row">
-            <label for="password">Password</label>
-            <input type="password" class="textbox" id="txt_pwd" name="txt_pwd" placeholder="Password" />
-        </div>
-        <input type="button" value="Submit" name="but_submit" id="but_submit" class="button"/>
-    </form>
 </body>
 
 </html>
